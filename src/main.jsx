57a078da5b50a926f247eabdd8f488600243bb8f
MihajaFi <127596566+MihajaFi@users.noreply.github.com>
2024-02-03 10:58:48 +0300

    feat: login ui
    * feat: add tailwind and react router
    
    * feat: add route to login
    
    * feat: image and logo for login page
    
    * feat: login for formekou ap
    
    * fix : reformate the code in login
    
    * fix: delete image
    
    * fix: change class to className
    
    * fix: delete import app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Login } from "./pages";
 import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
