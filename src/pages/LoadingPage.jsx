d07cb20e86d8b89f5426d5ae4d026c89889e83ce
Fifaliana <hei.fifaliana@gmail.com>
2024-02-05 22:15:58 +0300

    feat : design for loading page

---
+++ src/pages/LoadingPage.jsx
@@ -0,0 +1,32 @@
+export function LoadingPage() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="text-center flex flex-col items-center">
+        <svg
+          className="animate-spin h-12 w-12 mr-3 text-blue-500"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+        <p className="text-white text-lg">Loading...</p>
+        <p className="text-white text-lg">
+          This may take a few seconds, please don't close this page.
+        </p>
+      </div>
+    </div>
+  );
+}
