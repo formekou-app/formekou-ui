43ecfb085b0f4b122fe67505d125eaee3560c97f
Fifaliana <hei.fifaliana@gmail.com>
2024-02-07 17:14:40 +0300

    feat: header of the form

---
+++ src/components/FormBuilder/Header.jsx
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import { Box, TextField, Paper } from "@mui/material";
+export function Header({ title, description, setTitle, setDescription }) {
+  return (
+    <Fragment>
+    <Box sx={{ mb: 3 }}>
+      <Paper elevation={2} sx={{ p: 3, borderTop: "8px solid #010C80" }}>
+        <TextField
+          defaultValue={title}
+          onBlur={(e) => setTitle(e.target.value)}
+          variant="standard"
+          placeholder="Form Title"
+          name="title"
+          sx={{ mb: 3 }}
+          fullWidth
+        />
+        <TextField
+          name="description"
+          defaultValue={description}
+          onBlur={(e) => setDescription(e.target.value)}
+          variant="standard"
+          placeholder="Form Description"
+          fullWidth
+          sx={{ mb: 2 }}
+          multiline
+          rows={2}
+        />
+      </Paper>
+    </Box>
+  </Fragment>
+  )
+}
