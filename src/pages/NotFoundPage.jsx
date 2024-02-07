be465c4a50578d5505d789e2ff834f4ac8d1be71
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2024-02-07 17:06:47 +0300

    feat: migrate to tailwind components
    * chore: restyle login
    
    * chore: useTailwind button
    
    * style: format code

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
 
 import codeStatus from "../assets/images/404-2.png";
 import group from "../assets/images/Group.png";
 
 //TODO:  refactor all button using material tailwind button
 export function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="nf-container">
       <div className="resp-container relative">
@@ -16,9 +19,9 @@ export function NotFoundPage() {
             Sorry about that! Please visit our homepage to get where you need to
             go.
           </p>
-          <Link to="/">
-            <button className="other-btn">Take me there!</button>
-          </Link>
+          <Button onClick={() => navigate("/")} className="bg-main">
+            Take me there!
+          </Button>
         </div>
         <img src={codeStatus} />
       </div>
