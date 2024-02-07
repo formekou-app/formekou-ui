d32cddeecf19366e0a1b32f9371b8d6f356efba8
Fifaliana <hei.fifaliana@gmail.com>
2024-02-07 17:10:59 +0300

    feat: array of form element

---
+++ src/components/FormBuilder/constants.js
@@ -0,0 +1,27 @@
+export const formEl = [
+    {
+      label: "Text",
+      value: "text",
+    },
+    {
+      label: "TextArea",
+      value: "textarea",
+    },
+    {
+      label: "Number",
+      value: "number",
+    },
+    {
+      label: "Radio",
+      value: "radio",
+    },
+    {
+      label: "Date",
+      value: "date",
+    },
+    {
+      label: "Time",
+      value: "time",
+    }
+  ];
+  
