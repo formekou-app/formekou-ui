5d2c6cc633378833ca78786cb3905ce49b357b13
RickaPrincy <rckprincy@gmail.com>
2024-02-15 07:29:20 +0300

    chore: refactor menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,12 +1,14 @@
-import { Search as SearchIcon } from "@mui/icons-material";
+import { AccountCircleOutlined, Search as SearchIcon } from "@mui/icons-material";
+import { ViewMenu } from "./components/ViewMenu";
 
+//TODO: handle search here
 export function Menu(){
   return (
     <div 
-      className="py-5 px-2 w-[300px] bg-white fixed left-0 top-[50px]"
+      className="py-5 px-2 w-[275px] bg-white fixed left-0 top-[50px]"
       style={{height:"calc(100vh - 50px)"}}
     >
-      <div className="bg-gray-200 flex px-2 py-1 rounded-[5px] items-start gap-2">
+      <div className="bg-gray-200 flex mb-5 px-2 py-1 rounded-[5px] items-start gap-2">
         <SearchIcon className="text-main-black"/>
         <input 
           type="text" 
@@ -14,6 +16,9 @@ export function Menu(){
           placeholder="Search Form"
         />
       </div>
+      <ViewMenu label="PRIVATE">
+        hello
+      </ViewMenu>
     </div>
   )
 }
